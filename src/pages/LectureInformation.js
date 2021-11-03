078d375e78a6e1f2d9a00c1b2a64fb78ae858a7d
doldsimo <simon.dold@hs-furtwangen.de>
2021-11-03 18:27:49 +0100

    Update LectureInformation.js

--- src/pages/LectureInformation.js
+++ src/pages/LectureInformation.js
@@ -17,7 +17,7 @@ const LectureInformation = () => {
         <div className="lecture-information-container">
             <h1 className="center-lecture-information-text">{configData.subject}</h1>
             <h2 className="center-lecture-information-text">{configData.topic}</h2>
-            <p >Leineinheit Aufbau:</p>
+            <p >Lerneinheit Aufbau:</p>
             {configData.pages.map((page, index) => <p key={index}>{index + 1 + ". " + page.name}</p>)}
             <div className="center-lecture-information-text">
                 <button onClick={handleStartKlick} >Lerneinheit beginnen</button>
