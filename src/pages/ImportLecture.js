18051011a73f1237959aab3932223a2cf17a5d88
doldsimo <simon.dold@hs-furtwangen.de>
2021-12-01 21:04:23 +0100

    TASK: add download example lecture

--- src/pages/ImportLecture.js
+++ src/pages/ImportLecture.js
@@ -6,7 +6,9 @@ const ImportLecture = () => {
     return (
         <div className="import-lecture-container">
             <h1 className="center-import-lecture-text">Lern App</h1>
-            <p className="center-import-lecture-text">Importiere eine Lerneinheit</p>
+            <p className="center-import-lecture-text" style={{ marginBottom: "2.5px" }}>Importiere eine MDX Zip Lerneinheit</p>
+            <p className="center-import-lecture-text" style={{ fontSize: "small", marginTop: "2.5px", marginBottom: "" }}>(Beispiel Lerneinheit <a href="https://github.com//doldsimo/mdx-import-zipfile/blob/dev/ImportData/Mathematik%20Addition%20Subtraktion/Beispiellektion_Mathematik.zip?raw=true">Downloaden</a>)</p>
+
             <Reader />
         </div>
     )
